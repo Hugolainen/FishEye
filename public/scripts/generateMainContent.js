b4baadea4bebcc11ca32ad0d50772f475182f96c
hugoC42 <hugo.constant42@gmail.com>
2020-12-20 16:06:22 +0200

    Added the file to read the JSON data

--- public/scripts/generateMainContent.js
+++ public/scripts/generateMainContent.js
@@ -1,9 +1,9 @@
-let requestURL = 'FishEyeData.json';
+let requestURL = 'https://hugolainen.github.io/FishEye/public/data/FishEyeData.json';
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function() {
     const myData = request.response;
-
-  }
+    alert(myData.photographers[0].name)
+}
