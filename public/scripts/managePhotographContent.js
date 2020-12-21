e243f897dcd8780b4eaf68ff8fdfd0c33a5b9e89
hugoC42 <hugo.constant42@gmail.com>
2020-12-21 15:13:33 +0200

    Started working on automatic Media generation

---
+++ public/scripts/managePhotographContent.js
@@ -0,0 +1,65 @@
+const gallery = document.getElementById('photographerGallery');
+const photographID = 930;
+
+let requestURL = 'https://hugolainen.github.io/FishEye/public/data/FishEyeData.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    const myData = request.response;
+
+    const mediaList = myData.media;
+    for(var i=0; i<photographerList.length; i++){
+        gallery.appendChild(generateMediaCard(i));
+        //alert(myData.photographers[i].name)
+    }
+    
+
+
+    // Generate one photographer card
+    function generateMediaCard(index){
+        const mediaCard = document.createElement("div");
+        const mediaMedia = document.createElement("div");
+        const mediaDesc = document.createElement("div");
+        const mediaName = document.createElement("p");
+        const mediaPrice = document.createElement("p");
+        const mediaLike = document.createElement("p");
+    
+        mediaCard.classList.add("mediaCard"); 
+        mediaMedia.classList.add("mediaCard__image"); 
+        mediaDesc.classList.add("mediaCard__desc"); 
+        mediaName.classList.add("mediaCard__desc__name"); 
+        mediaPrice.classList.add("mediaCard__desc__number"); 
+        mediaLike.classList.add("mediaCard__desc__number"); 
+
+        const newMedia = mediaList[index];
+        mediaMedia.innerHTML = "<img class=\"modalMedia_open\" src=\"public/img/media/" + newMedia.image + " alt=" + newMedia.alt + ">";
+        mediaName.innerHTML = newMedia.alt;
+        mediaPrice.innerHTML = newMedia.price + " $";
+        mediaLike.innerHTML = newMedia.likes + " <i class=\"fas fa-heart\"></i>";
+    
+        mediaDesc.appendChild(mediaName);
+        mediaDesc.appendChild(mediaPrice);
+        mediaDesc.appendChild(mediaLike);
+        mediaCard.appendChild(mediaMedia);
+        mediaCard.appendChild(mediaDesc);
+
+        return mediaCard;
+    }
+}
+
+
+/*
+<div class="mediaCard">
+    <div class="mediaCard__image">  
+        <img id="modalMedia_open" src="public/img/photographersIDphotos/MimiKeel.jpg" alt="">
+    </div>
+    <div class="mediaCard__desc"> 
+        <p class="mediaCard__desc__name"> Rainbow </p>
+        <p class="mediaCard__desc__number"> 70 $ </p>
+        <p class="mediaCard__desc__number"> 12 <i class="fas fa-heart"></i> </p>
+    </div>
+</div>
+*/
+
