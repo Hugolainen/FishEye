27d31cf3931c0c15ff540846f3611c1a98a9ceea
hugoC42 <hugo.constant42@gmail.com>
2020-12-22 10:13:12 +0200

    Added media Img First version of Media Card automatic generation

--- public/scripts/managePhotographContent.js
+++ public/scripts/managePhotographContent.js
@@ -1,4 +1,4 @@
-const gallery = document.getElementById('photographerGallery');
+const gallery = document.getElementById('mediaGallery');
 const photographID = 930;
 
 let requestURL = 'https://hugolainen.github.io/FishEye/public/data/FishEyeData.json';
@@ -9,13 +9,74 @@ request.send();
 request.onload = function() {
     const myData = request.response;
 
-    const mediaList = myData.media;
-    for(var i=0; i<photographerList.length; i++){
-        gallery.appendChild(generateMediaCard(i));
+    const fullMediaList = myData.media;
+    const photographerMediaList = getPhotographerMediaList(fullMediaList);
+
+
+    for(var i=0; i<photographerMediaList.length; i++){
+        gallery.appendChild(generateMediaCard(photographerMediaList[i]));
         //alert(myData.photographers[i].name)
     }
     
 
+    function getPhotographerMediaList(baseMediaList)
+    {
+        let mediaList = [];
+        for(var i=0; i<baseMediaList.length;i++)
+        {
+            if(baseMediaList[i].photographerId == photographID)
+            {
+                mediaList.push(i);
+            }
+        }
+        return mediaList;
+    }
+
+    function orderMediaList(orderType, photographerMediaList){
+        let orderedList = photographerMediaList;
+        let continueSort = true;
+        let temp;
+        if(orderType=="date")
+        {
+            
+        }
+        else if(orderType=="price")
+        {
+            while(continueSort)
+            {
+                continueSort = false;
+                for(var i=0; i<orderedList.length-1; i++)
+                {
+                    if(mediaList[orderedList[i]].price > mediaList[orderedList[i+1]].price){
+                        temp = orderedList[i];
+                        orderedList[i] = orderedList[i+1];
+                        orderedList[i+1] = temp;
+                        continueSort = true;
+                    }
+                }
+            }
+        }
+        else{
+            while(continueSort)
+            {
+                continueSort = false;
+                for(var i=0; i<orderedList.length-1; i++)
+                {
+                    if(mediaList[orderedList[i]].likes < mediaList[orderedList[i+1]].likes){
+                        temp = orderedList[i];
+                        orderedList[i] = orderedList[i+1];
+                        orderedList[i+1] = temp;
+                        continueSort = true;
+                    }
+                }
+            }
+        }
+    }
+
+    // Generate the photographer profil and footer numbers
+    function generateProfile(){
+        return false;
+    }
 
     // Generate one photographer card
     function generateMediaCard(index){
@@ -33,7 +94,7 @@ request.onload = function() {
         mediaPrice.classList.add("mediaCard__desc__number"); 
         mediaLike.classList.add("mediaCard__desc__number"); 
 
-        const newMedia = mediaList[index];
+        const newMedia = fullMediaList[index];
         mediaMedia.innerHTML = "<img class=\"modalMedia_open\" src=\"public/img/media/" + newMedia.image + " alt=" + newMedia.alt + ">";
         mediaName.innerHTML = newMedia.alt;
         mediaPrice.innerHTML = newMedia.price + " $";
